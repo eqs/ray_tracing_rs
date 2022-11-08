88c3562a0d2edec0a54a14e9440208643e75746a
eqs <murashige.satoshi.mi1@gmail.com>
2022-11-08 17:49:26 +0900

    Antialiasing

--- examples/materials.rs
+++ examples/materials.rs
@@ -1,5 +1,6 @@
+use rand::prelude::*;
 use ray_tracing_utils::math::{Vec3, Point3, Color, Ray};
-use ray_tracing_utils::color::write_pixel;
+use ray_tracing_utils::color::write_pixel_sample;
 use ray_tracing_utils::hittable::{Sphere, Hittable, HittableList};
 use ray_tracing_utils::camera::Camera;
 
@@ -24,6 +25,7 @@ fn main() {
     let aspect_ratio = 16.0 / 9.0;
     let image_width = 512;
     let image_height = (image_width as f32 / aspect_ratio) as i32;
+    let samples_per_pixel = 100;
 
     // World
 
@@ -41,17 +43,25 @@ fn main() {
 
     // Render
 
+    let mut rng = thread_rng();
+
     println!("P3");
     println!("{} {}", image_width, image_height);
     println!("255");
 
     for i in (0..image_height).rev() {
         for j in 0..image_width {
-            let u = j as f32 / (image_width - 1) as f32;
-            let v = i as f32 / (image_height - 1) as f32;
-            let ray = camera.get_ray(u, v);
-            let color = ray_color(&ray, &world);
-            write_pixel(color);
+            let mut pixel_color: Color = Color::new(0.0, 0.0, 0.0);
+            for _ in 0..samples_per_pixel {
+                let j_offset: f32 = rng.gen();
+                let i_offset: f32 = rng.gen();
+                let u = (j as f32 + j_offset) / (image_width - 1) as f32;
+                let v = (i as f32 + i_offset) / (image_height - 1) as f32;
+                let ray = camera.get_ray(u, v);
+                pixel_color = pixel_color + ray_color(&ray, &world);
+            }
+
+            write_pixel_sample(pixel_color, samples_per_pixel);
         }
     }
 }
--- ray_tracing_utils/src/color.rs
+++ ray_tracing_utils/src/color.rs
@@ -6,3 +6,11 @@ pub fn write_pixel(color: Color) {
     let b = (clamp(color.z, 0.0, 0.999) * 256.0) as i32;
     println!("{} {} {}", r, g, b);
 }
+
+pub fn write_pixel_sample(color: Color, samples_per_pixel: i32) {
+    let s = 1.0 / samples_per_pixel as f32;
+    let r = (clamp(color.x * s, 0.0, 0.999) * 256.0) as i32;
+    let g = (clamp(color.y * s, 0.0, 0.999) * 256.0) as i32;
+    let b = (clamp(color.z * s, 0.0, 0.999) * 256.0) as i32;
+    println!("{} {} {}", r, g, b);
+}
